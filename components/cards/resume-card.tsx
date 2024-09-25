e8f28e0cc9013a785cd6fcffff20942170cd35ff
apykwan <apykwan@hotmail.com>
2024-09-25 02:08:05 -0700

    completed resume card

--- components/cards/resume-card.tsx
+++ components/cards/resume-card.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+
+import PersonalDetails from '@/components/preview/personal-details';
 import { type ResumeType } from '@/context/resume';
 
 type ResumeCardProps = {
@@ -6,17 +9,13 @@ type ResumeCardProps = {
 
 export default function ResumeCard({ resume }: ResumeCardProps) {
   return (
-    <div 
-      className="shadow-lg h-[175px] w-full rounded-xl p-5 border-t-[20px]"
-      style={{ borderColor: resume?.themeColor }}
-    >
-      <ul>
-        <li>Personal Details</li>
-        <li>Summary</li>
-        <li>Expereince</li>
-        <li>Education</li>
-        <li>Skills</li>
-      </ul>
-    </div>
+    <Link href={`/dashboard/resume/edit/${resume._id}`}>
+      <div 
+        className="shadow-lg h-[175px] w-full rounded-xl p-5 border-t-[20px]"
+        style={{ borderColor: resume?.themeColor }}
+      >
+        <PersonalDetails resume={resume} />
+      </div>
+    </Link>
   );
 }
--- components/preview/personal-details.tsx
+++ components/preview/personal-details.tsx
@@ -15,7 +15,10 @@ export default function PersonalDetails({ resume }: PreviewCardProps) {
       </h2>
       <h2 className="text-center text-sm font-medium">{resume.job}</h2>
       <h2 className="text-center text-sm font-medium">{resume.address}</h2>
-      <div className="flex justify-between"></div>
+      <div className="flex justify-between">
+        <div className="font-normal text-xs">{resume.phone}</div>
+        <div className="font-normal text-xs">{resume.email}</div>
+      </div>
     </>
   );
 }
